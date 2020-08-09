725868191e9e72eef442b49bade4bc2a7a72fc8f
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2020-08-09 04:42:59 +0200

    good to test

--- pcsx2/IPC.cpp
+++ pcsx2/IPC.cpp
@@ -15,7 +15,7 @@ void SocketIPC::Start() {
 
     m_sock = socket(AF_UNIX, SOCK_STREAM, 0);
     if (m_sock < 0) {
-	    Console.WriteLn( Color_StrongBlue, "IPC: Cannot open socket! Shutting down...\n" );
+	    Console.WriteLn( Color_Red, "IPC: Cannot open socket! Shutting down...\n" );
         return;
     }
     server.sun_family = AF_UNIX;
@@ -25,7 +25,7 @@ void SocketIPC::Start() {
     // freed even if we didn't close correctly the loop
     unlink(SOCKET_NAME);
     if (bind(m_sock, (struct sockaddr *) &server, sizeof(struct sockaddr_un))) {
-	    Console.WriteLn( Color_StrongBlue, "IPC: Error while binding to socket! Shutting down...\n" );
+	    Console.WriteLn( Color_Red, "IPC: Error while binding to socket! Shutting down...\n" );
         return;
     }
 
@@ -41,13 +41,13 @@ void SocketIPC::SocketThread() {
     while(true) {
         m_msgsock = accept(m_sock, 0, 0);
         if (m_msgsock == -1) {
-	            Console.WriteLn( Color_StrongBlue, "IPC: Connection to socket broken! Shutting down...\n" );
+	            Console.WriteLn( Color_Red, "IPC: Connection to socket broken! Shutting down...\n" );
                 return;
             }
         else {
             bzero(buf, sizeof(buf));
             if (read(m_msgsock, buf, 1024) < 0) {
-	            Console.WriteLn( Color_StrongBlue, "IPC: Connection to socket broken! Shutting down...\n" );
+	            Console.WriteLn( Color_Red, "IPC: Connection to socket broken! Shutting down...\n" );
                 return;
             }
             else {
@@ -70,5 +70,32 @@ SocketIPC::~SocketIPC() {
 char* SocketIPC::ParseCommand(char *buf) {
     // TODO: Actually parse different IPC events
     printf("-->%s\n", buf);
+    int opcode = (int)buf[0];
+    switch (opcode) {
+        case MsgRead8: 
+            printf("read 8!"); 
+            break;
+        case MsgRead16: 
+            printf("read 16!"); 
+            break;
+        case MsgRead32: 
+            printf("read 32!"); 
+            break;
+        case MsgRead64: 
+            printf("read 64!"); 
+            break;
+        case MsgWrite8: 
+            printf("write 8!"); 
+            break;
+        case MsgWrite16: 
+            printf("write 16!"); 
+            break;
+        case MsgWrite32: 
+            printf("write 32!"); 
+            break;
+        case MsgWrite64: 
+            printf("write 64!"); 
+            break;
+    }
     return buf;
 }
